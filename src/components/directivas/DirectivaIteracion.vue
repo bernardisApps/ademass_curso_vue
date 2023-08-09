66257fcd49d9a0f92451e2ccbfbf3d39ffcfea6b
bernardisApps <matias.bernardis@hotmail.com>
2023-08-09 13:03:27 -0300

    actualizacion del componente directivaiteracion

--- src/components/directivas/DirectivaIteracion.vue
+++ src/components/directivas/DirectivaIteracion.vue
@@ -1,12 +1,18 @@
 <template>
-    <h1 v-for="(nombre, index) in nombres" :key="index"> {{ nombre }} </h1>
+    <div class="container">
+        <div class="row mx-auto">
+            <div class="col-md-6 mx-auto">
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-action" v-for="(nombre, index) in nombres" :key="index">{{ nombre }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
-    import {ref} from 'vue'
-    let nombres = ref(['matias', 'bruno', 'florencia']);
+import { ref } from 'vue'
+let nombres = ref(['matias', 'bruno', 'florencia']);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
